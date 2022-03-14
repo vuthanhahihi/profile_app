e77fda08e39f0f760ad000cbac1097beb2360499
vutha <vuthanhahihi>
2022-03-14 10:47:26 +0700

    responsive intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import Topbar from "./components/topbar/Topbar";
-import Intro from "./components/intro/Intro"
-import Portfolio from "./components/portfolio/Portfolio"
-import Contact from "./components/contact/Contact"
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
 import Menu from "./components/menu/Menu";
-import "./app.scss"
-import { useState, useEffect} from "react";
+import "./app.scss";
+import { useState, useEffect } from "react";
 
 function App() {
-
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
 
   useEffect(() => {
     fetch("https://622827009fd6174ca81c3475.mockapi.io/api/information")
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
         (result) => {
           setIsLoaded(true);
@@ -26,26 +25,24 @@ function App() {
           setIsLoaded(true);
           setError(error);
         }
-      )
-  }, [])
+      );
+  }, []);
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
     return <div>Loading...</div>;
-  }else
-
-
-  return (
-    <div className="app">
-     <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} items={items}/>
-     <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-     <div className="sections">
-       <Intro items={items}/>
-       <Portfolio items={items}/>
-       <Contact items={items}/>
-       </div>
-    </div>
-  );
+  } else
+    return (
+      <div className="app">
+        <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} items={items} />
+        <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <div className="sections">
+          <Intro items={items} />
+          <Portfolio items={items} />
+          <Contact items={items} />
+        </div>
+      </div>
+    );
 }
 
 export default App;
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,16 +3,14 @@ import { ArrowDropDownCircleOutlined } from "@material-ui/icons";
 
 export default function Intro({ items }) {
   return (
-    <div className="intro" id="intro">
-      <div className="left">
-        <div className="imgContainer">
+    <div className="container-fluid" id="intro">
+      <div className="row">
+        <div className="col-md-6 imgContainer">
           {items.map((item, index) => (
             <img key={index} src={item.url} alt="" />
           ))}
         </div>
-      </div>
-      <div className="right">
-        <div className="wrapper">
+        <div className="col-md-6 wrapper">
           <h2>Xin chào, mình là</h2>
           {items.map((item) => (
             <span key={item.id}>
